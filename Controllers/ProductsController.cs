01168611b95c3001335ffbb8e757d5ac779a232c
SupriyaY17 <88817304+SupriyaY17@users.noreply.github.com>
2021-12-17 19:12:48 -0800

    Initial commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DeltaCore.Models;
+
+namespace DeltaCore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public ActionResult product()
+        {
+            var products = new List<ProductsService>()
+            {
+                new ProductsService { Id = 1001,Name = "Tools Gears" , Description = "Gear Tools production specification"},
+                new ProductsService { Id = 2001,Name = "panels" , Description = "Switch Gear Panels"},
+                new ProductsService { Id = 2008,Name = "DocTonar" , Description ="Document Solar Objects" }
+
+            };
+            return View(products);
+        }
+    }
+}
